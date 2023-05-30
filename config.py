a5a3f85fa40fce94daf7f7ce4d52054518211652
Iurii Trokhachev <126320741+tchefee@users.noreply.github.com>
2023-05-30 12:43:53 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -3,11 +3,21 @@ TOKEN = '6066186925:AAHXF-pgAJsG2GsN-JAvk8dyY8OdYj1evTA'
 CURRENCY_CODES = {
     'евро': 'EUR',
     'доллар': 'USD',
-    'рубль': 'RUB'
+    'рубль': 'RUB',
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'дугикоин': 'DOGE',
+    'фунт': 'GBP',
+    'иена': 'JPY'
 }
 
 CURRENCY_NAMES = {
     'EUR': 'евро',
     'USD': 'доллар',
-    'RUB': 'рубль'
+    'RUB': 'рубль',
+    'BTC': 'биткоин',
+    'ETH': 'эфириум',
+    'DOGE': 'дугикоин',
+    'GBP': 'фунт',
+    'JPY': 'иена'
 }
